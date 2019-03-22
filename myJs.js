cebba7ac06b2317e1945386653b92ad5cffe8a76
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2019-03-22 09:47:59 -0700

    Add files via upload

---
+++ myJs.js
@@ -0,0 +1,13 @@
+function lewisIndoor(){
+  var lewis = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQfkq27Oy5QrFPbB5TZYlrqwx8WyJUmR8qBQlcBOe-2wmXvb-MBoR6PMt_uJG9Gh4u1J1vJFORKwlAF/pubhtml?gid=657807412&single=true";
+  document.getElementById("iframe").src = lewis;
+  document.getElementById("iframe").height = "600px";
+  document.getElementById("iframe").width = "100%";
+}
+
+function topTimes(){
+  var topTimes = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQfkq27Oy5QrFPbB5TZYlrqwx8WyJUmR8qBQlcBOe-2wmXvb-MBoR6PMt_uJG9Gh4u1J1vJFORKwlAF/pubhtml?gid=1448138541&single=true";
+  document.getElementById("iframe").src = topTimes;
+  document.getElementById('iframe').height = "350px";
+  document.getElementById("iframe").width = "100%";
+}
